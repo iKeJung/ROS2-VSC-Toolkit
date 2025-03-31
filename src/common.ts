60688fcc4b38b26c271e87fe23f303994e2b2ac1
Henrique Jung de Carvalho <henrique.jung@senairs.org.br>
2025-03-31 18:27:14 -0300

    feat: source files from configuration

--- src/common.ts
+++ src/common.ts
@@ -1,2 +1,31 @@
-export const ext_name = 'ros2-topic-viewer';
-export const ext_title = 'ROS2 Topic Viewer';
+import * as vscode from "vscode";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+
+export const ext_name = "ros2-topic-viewer";
+export const ext_title = "ROS2 Topic Viewer";
+
+function getSourceCommand(): string | null {
+    const config = vscode.workspace.getConfiguration(ext_name);
+    const files = config.get<Array<string>>("filesToSource");
+    if (!files || files.length === 0) {
+        return null;
+    }
+    let sourceCommand = "";
+    for (const file of files!) {
+        sourceCommand += `source ${file} && `;
+    }
+    return sourceCommand;
+}
+export function addSourceCommand(command: string): string {
+    const sourceCommand = getSourceCommand() || "";
+    return `bash -c '${sourceCommand}${command}'`;
+}
+
+export function spawnWithSource(command: string, args: readonly string[]): ChildProcessWithoutNullStreams {
+    var rosCommand = `${getSourceCommand() || ""}${command}`;
+    for (const arg of args) {
+        rosCommand += " " + arg;
+    }
+
+    return spawn("bash", ["-c", rosCommand]);
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,85 +1,75 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import { ROS2TopicsProvider } from './topicProvider';
-import { ext_title, ext_name } from './common';
+import { ROS2TopicsProvider } from "./topicProvider";
+import { ext_title, ext_name, addSourceCommand, spawnWithSource } from "./common";
 
-import { spawn } from 'child_process';
-import { exec } from 'child_process';
+import { exec } from "child_process";
 
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
     const ros2TopicsProvider = new ROS2TopicsProvider();
-    vscode.window.registerTreeDataProvider('ros2TopicsView', ros2TopicsProvider);
+    vscode.window.registerTreeDataProvider("ros2TopicsView", ros2TopicsProvider);
 
     let panelList: vscode.WebviewPanel[] = [];
     const max_panels = 4;
 
     const new_disposable = vscode.commands.registerCommand(`${ext_name}.refreshTopics`, () => {
         ros2TopicsProvider.refresh();
-        vscode.window.showInformationMessage(ext_title + ': Topics refreshed!');
-        
+        vscode.window.showInformationMessage(ext_title + ": Topics refreshed!");
     });
 
     const showMessagesDisposable = vscode.commands.registerCommand(`${ext_name}.showMessages`, (topic: string) => {
-        
         const config = vscode.workspace.getConfiguration(ext_name);
-        
-        if(panelList.length>=max_panels && config.get<boolean>('panelLimitSetting')){
-            vscode.window.showErrorMessage('Reached maximum number of panels! Please close a panel to open a new one.');
+
+        if (panelList.length >= max_panels && config.get<boolean>("panelLimitSetting")) {
+            vscode.window.showErrorMessage("Reached maximum number of panels! Please close a panel to open a new one.");
             return;
         }
 
-        let existingPanel = panelList.find(p => p.title === `Messages for ${topic}`);
+        let existingPanel = panelList.find((p) => p.title === `Messages for ${topic}`);
 
         if (existingPanel) {
-
             existingPanel.reveal(vscode.ViewColumn.One);
             return;
-
         }
-        
+
         const panel = vscode.window.createWebviewPanel(
-            'topicMessages',
+            "topicMessages",
             `Messages for ${topic}`,
             vscode.ViewColumn.One,
-            { enableScripts: true }
+            {
+                enableScripts: true,
+            }
         );
 
         panelList.push(panel);
         showTopicMessages(topic, panel, panelList);
 
-        if(ros2TopicsProvider.getAdvancedMode()){
+        if (ros2TopicsProvider.getAdvancedMode()) {
             showAdvancedMessages(topic, panel);
         }
-        
     });
 
     const toggleAdvancedDisposable = vscode.commands.registerCommand(`${ext_name}.toggleAdvanced`, () => {
-
         const advancedMode = ros2TopicsProvider.toggleAdvanced();
-        if(advancedMode){
-            vscode.window.showInformationMessage(ext_title + ': Advanced mode enabled! Close panels to apply.');
-        }
-        else{
-            vscode.window.showInformationMessage(ext_title + ': Advanced mode disabled! Close panels to apply.');
+        if (advancedMode) {
+            vscode.window.showInformationMessage(ext_title + ": Advanced mode enabled! Close panels to apply.");
+        } else {
+            vscode.window.showInformationMessage(ext_title + ": Advanced mode disabled! Close panels to apply.");
         }
 
-        // TODO: in impostazioni aggiungere un flag per chiudere automaticamente i pannelli
         // while(panelList.length>0){
         //     const panel = panelList.pop();
         //     if (panel) {
         //         panel.dispose();
         //     }
         // }
-
     });
 
     const togglePanelLimitDisposable = vscode.commands.registerCommand(`${ext_name}.togglePanelLimit`, () => {
-
         togglePanelLimit();
-
     });
 
     context.subscriptions.push(new_disposable);
@@ -88,208 +78,192 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(togglePanelLimitDisposable);
 }
 
-async function togglePanelLimit(){
-
-    try{
+async function togglePanelLimit() {
+    try {
         const config = vscode.workspace.getConfiguration(ext_name);
-        const panelLimit = config.get<boolean>('panelLimitSetting');
-        await config.update('panelLimitSetting', !panelLimit, vscode.ConfigurationTarget.Global);
-        vscode.window.showInformationMessage('The panel limit setting is now set to: ' + config.get<boolean>('panelLimitSetting'));
-    }catch(err){
-        vscode.window.showErrorMessage('Error: ' + err);
+        const panelLimit = config.get<boolean>("panelLimitSetting");
+        await config.update("panelLimitSetting", !panelLimit, vscode.ConfigurationTarget.Global);
+        vscode.window.showInformationMessage(
+            "The panel limit setting is now set to: " + config.get<boolean>("panelLimitSetting")
+        );
+    } catch (err) {
+        vscode.window.showErrorMessage("Error: " + err);
     }
-
 }
 
 async function updateInfoPanel(topic: string, panel: vscode.WebviewPanel) {
-
-    if(panel.visible === false) {
+    if (panel.visible === false) {
         return;
     }
 
-    const process_secundary = exec('ros2 topic info ' + topic + ' --verbose');
+    const process_secondary = exec(addSourceCommand("ros2 topic info " + topic + " --verbose"));
 
-    process_secundary.stdout?.on('data', (data) => {
-
-        panel.webview.postMessage({ command: 'pushInfo', message: data.toString() });
-        process_secundary.kill();
-
+    process_secondary.stdout?.on("data", (data) => {
+        panel.webview.postMessage({ command: "pushInfo", message: data.toString() });
+        process_secondary.kill();
     });
 
-    process_secundary.stderr?.on('data', (data) => {
-
-        console.error(`Error from process secundary: ${data}`);
-        panel.webview.postMessage({ command: 'error', message: data });
-
+    process_secondary.stderr?.on("data", (data) => {
+        console.error(`Error from process secondary: ${data}`);
+        panel.webview.postMessage({ command: "error", message: data });
     });
 }
 
 async function showTopicMessages(topic: string, panel: vscode.WebviewPanel, panelList: vscode.WebviewPanel[]) {
-
-	panel.onDidDispose(() => {
-
-        console.log('Disposing panel');
-        // console.log('indexof: ' + panelList.indexOf(panel));
+    panel.onDidDispose(() => {
+        console.log("Disposing panel");
+        // console.log('index of: ' + panelList.indexOf(panel));
 
         panelList.splice(panelList.indexOf(panel), 1);
 
-		if (process){
+        if (process) {
             // console.log('Sending SIGTERM to process');
-            process.kill('SIGCONT');
-            process.kill('SIGTERM');  
-		}
-        if (process_secundary){
-            process_secundary.kill('SIGCONT');
-            process_secundary.kill('SIGTERM');
+            process.kill("SIGCONT");
+            process.kill("SIGTERM");
         }
-
-	});
+        if (process_secondary) {
+            process_secondary.kill("SIGCONT");
+            process_secondary.kill("SIGTERM");
+        }
+    });
 
     panel.webview.html = getWebviewContent(topic);
 
-    const process = spawn('ros2', ['topic', 'echo', topic]);
-	const process_secundary = exec('ros2 topic info ' + topic + ' --verbose');
+    const process = spawnWithSource("ros2", ["topic", "echo", topic]);
+    const process_secondary = exec(addSourceCommand("ros2 topic info " + topic + " --verbose"));
 
-    process.stdout?.on('data', (data) => {
-
+    process.stdout?.on("data", (data) => {
         //if data is too big, truncate it:
         if (data.length > 10000) {
             data = data.slice(0, 10000);
-            data = 'Unable to visualize the entire message. The message is too big. \n\n';
-            panel.webview.postMessage({ command: 'error', message: data.toString() });
+            data = "Unable to visualize the entire message. The message is too big. \n\n";
+            panel.webview.postMessage({ command: "error", message: data.toString() });
             // process.kill();
             return;
         }
-        if (data.toString().includes('fastrtps_port')) { return; } // ignore fastrtps_port messages lock and load
-        panel.webview.postMessage({ command: 'update', message: data.toString() });
-        
+        if (data.toString().includes("fastrtps_port")) {
+            return;
+        } // ignore fastrtps_port messages lock and load
+        panel.webview.postMessage({ command: "update", message: data.toString() });
     });
 
-    process.stderr?.on('data', (data) => {
-
+    process.stderr?.on("data", (data) => {
         console.error(`Error: ${data}`);
-        panel.webview.postMessage({ command: 'error', message: data });
-
+        panel.webview.postMessage({ command: "error", message: data });
     });
 
-    process.on('exit', (code) => {
-
+    process.on("exit", (code) => {
         console.log(`Process exited with code: ${code}`);
-        panel.webview.postMessage({ command: 'exit', message: `The command has exited with code ${code}.` });
+        panel.webview.postMessage({ command: "exit", message: `The command has exited with code ${code}.` });
         // panel.dispose();
-
     });
 
-	process_secundary.stdout?.on('data', (data) => {
-        
-        if (data.toString().includes('fastrtps_port')) { return; }
-		panel.webview.postMessage({ command: 'pushInfo', message: data });
-		process_secundary.kill();
+    process_secondary.stdout?.on("data", (data) => {
+        if (data.toString().includes("fastrtps_port")) {
+            return;
+        }
+        panel.webview.postMessage({ command: "pushInfo", message: data });
+        process_secondary.kill();
+    });
 
-	});
-
-    process_secundary.stderr?.on('data', (data) => {
-
-        console.error(`Error from process secundary: ${data}`);
-        panel.webview.postMessage({ command: 'error', message: data });
-
+    process_secondary.stderr?.on("data", (data) => {
+        console.error(`Error from process secondary: ${data}`);
+        panel.webview.postMessage({ command: "error", message: data });
     });
 
     panel.onDidChangeViewState((event) => {
-
         if (event.webviewPanel.visible) {
-            if(process) {process.kill('SIGCONT');}
-            if(process_secundary) {process_secundary.kill('SIGCONT');}
+            if (process) {
+                process.kill("SIGCONT");
+            }
+            if (process_secondary) {
+                process_secondary.kill("SIGCONT");
+            }
             updateInfoPanel(topic, panel);
-        }else{
-            if(process) {process.kill('SIGSTOP');}
-            if(process_secundary){ process_secundary.kill('SIGSTOP');}
+        } else {
+            if (process) {
+                process.kill("SIGSTOP");
+            }
+            if (process_secondary) {
+                process_secondary.kill("SIGSTOP");
+            }
         }
     });
 
-    panel.webview.postMessage({ command: 'hideAdvanced', message: 'Advanced mode disabled!' });
-
-            
+    panel.webview.postMessage({ command: "hideAdvanced", message: "Advanced mode disabled!" });
 }
 
 async function showAdvancedMessages(topic: string, panel: vscode.WebviewPanel) {
-
     panel.webview.html = getWebviewContent(topic);
     // console.log('showing advanced messages');
-    const process = spawn('ros2', ['topic', 'hz', topic]);
-    const process_secundary = spawn('ros2', ['topic', 'bw', topic]);
+    const process = spawnWithSource("ros2", ["topic", "hz", topic]);
+    const process_secondary = spawnWithSource("ros2", ["topic", "bw", topic]);
 
-    panel.webview.postMessage({ command: 'showAdvanced', message: 'Advanced mode enabled!' });
+    panel.webview.postMessage({ command: "showAdvanced", message: "Advanced mode enabled!" });
 
     panel.onDidDispose(() => {
-            
-            if (process){
-                // console.log('Sending SIGTERM to process');
-                process.kill('SIGCONT');
-                process.kill('SIGTERM');  
-            }
-            if (process_secundary){
-                process_secundary.kill('SIGCONT');
-                process_secundary.kill('SIGTERM');
-            }
-
-        });
-
-    process.stdout?.on('data', (data) => {
-        // console.log(`Data: ${data}`);
-        if (data.toString().includes('fastrtps_port')) { return; }
-        panel.webview.postMessage({ command: 'updateHz', message: data.toString() });
-
-    });
-
-    process.stderr?.on('data', (data) => {
-        console.error(`Error: ${data}`);
-        panel.webview.postMessage({ command: 'error', message: data });
-
-    });
-
-    process.on('exit', (code) => {
-
-        console.log(`Process exited with code: ${code}`);
-        panel.webview.postMessage({ command: 'exit', message: `The command has exited with code ${code}.` });
-        // panel.dispose();
-
-    });
-
-    process_secundary.stdout?.on('data', (data) => {
-    
-        // console.log(`Data: ${data}`);
-        if (data.toString().includes('fastrtps_port')) { return; }
-        if(data.toString().includes('Subscribed')) { return; }
-        panel.webview.postMessage({ command: 'updateBw', message: data.toString() });
-
-    });
-
-    process_secundary.stderr?.on('data', (data) => {
-
-        console.error(`Error: ${data}`);
-        panel.webview.postMessage({ command: 'error', message: data });
-
-    });
-
-    panel.onDidChangeViewState((event) => {
-
-        if(event.webviewPanel.visible) {
-            process.kill('SIGCONT');
-            process_secundary.kill('SIGCONT');
-            panel.webview.postMessage({ command: 'showAdvanced', message: 'Advanced mode enabled!' });
-            return;
-        }else{
-            process.kill('SIGSTOP');
-            process_secundary.kill('SIGSTOP');
+        if (process) {
+            // console.log('Sending SIGTERM to process');
+            process.kill("SIGCONT");
+            process.kill("SIGTERM");
+        }
+        if (process_secondary) {
+            process_secondary.kill("SIGCONT");
+            process_secondary.kill("SIGTERM");
         }
     });
 
+    process.stdout?.on("data", (data) => {
+        // console.log(`Data: ${data}`);
+        if (data.toString().includes("fastrtps_port")) {
+            return;
+        }
+        panel.webview.postMessage({ command: "updateHz", message: data.toString() });
+    });
+
+    process.stderr?.on("data", (data) => {
+        console.error(`Error: ${data}`);
+        panel.webview.postMessage({ command: "error", message: data });
+    });
+
+    process.on("exit", (code) => {
+        console.log(`Process exited with code: ${code}`);
+        panel.webview.postMessage({ command: "exit", message: `The command has exited with code ${code}.` });
+        // panel.dispose();
+    });
+
+    process_secondary.stdout?.on("data", (data) => {
+        // console.log(`Data: ${data}`);
+        if (data.toString().includes("fastrtps_port")) {
+            return;
+        }
+        if (data.toString().includes("Subscribed")) {
+            return;
+        }
+        panel.webview.postMessage({ command: "updateBw", message: data.toString() });
+    });
+
+    process_secondary.stderr?.on("data", (data) => {
+        console.error(`Error: ${data}`);
+        panel.webview.postMessage({ command: "error", message: data });
+    });
+
+    panel.onDidChangeViewState((event) => {
+        if (event.webviewPanel.visible) {
+            process.kill("SIGCONT");
+            process_secondary.kill("SIGCONT");
+            panel.webview.postMessage({ command: "showAdvanced", message: "Advanced mode enabled!" });
+            return;
+        } else {
+            process.kill("SIGSTOP");
+            process_secondary.kill("SIGSTOP");
+        }
+    });
 }
 
 function getWebviewContent(topic: string): string {
-    const htmlPath = path.join(__dirname,'..', 'media', 'webview.html');
-    let htmlContent = fs.readFileSync(htmlPath, 'utf8');
-    htmlContent = htmlContent.replace('${topic}', topic);
+    const htmlPath = path.join(__dirname, "..", "media", "webview.html");
+    let htmlContent = fs.readFileSync(htmlPath, "utf8");
+    htmlContent = htmlContent.replace("${topic}", topic);
     return htmlContent;
 }
